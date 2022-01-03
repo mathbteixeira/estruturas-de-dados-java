49ce3c15d9785217a39a899d550dfdad8038c7f9
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-03 21:33:24 -0200

    Aula 05 - Obter elemento de uma posição

--- src/com/loiane/estruturadados/vetor/Vetor.java
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -12,27 +12,6 @@ public class Vetor {
         this.tamanho = 0;
     }
 
-    public int getTamanho() {
-        return this.tamanho;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append("[");
-
-        for (int i = 0; i < this.tamanho - 1; i++) {
-            s.append(this.elementos[i]);
-            s.append(", ");
-        }
-        if (this.tamanho > 0) {
-            s.append(this.elementos[this.tamanho - 1]);
-        }
-        s.append("]");
-
-        return s.toString();
-    }
-
     /*public void adiciona(String elemento) {
         for (int i = 0; i < this.elementos.length; i++) {
             if (this.elementos[i] == null) {
@@ -59,5 +38,34 @@ public class Vetor {
             return true;
         }
         return false;
+    }
+
+    public String busca(int posicao) {
+        // Posições válidas: 0 até tamanho
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        return this.elementos[posicao];
+    }
+
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+        s.append("]");
+
+        return s.toString();
     }
 }
---
+++ src/com/loiane/estruturadados/vetor/teste/Aula05.java
@@ -0,0 +1,17 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula05 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("elemento 1"); //0
+        vetor.adiciona("elemento 2"); //1
+        vetor.adiciona("elemento 3"); //2
+
+        System.out.println(vetor.busca(2)); //OK
+        System.out.println(vetor.busca(15)); //Exception
+        System.out.println(vetor.busca(-1)); //Exception
+    }
+}
