ff540717129e1b32e8678650b108af7004424026
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-04 21:51:13 -0200

    Aula 08 - Aumentar capacidade do Vetor

--- src/com/loiane/estruturadados/vetor/Vetor.java
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -32,6 +32,7 @@ public class Vetor {
     }*/
 
     public boolean adiciona(String elemento) {
+        this.aumentaCapacidade();
         if (this.tamanho < this.elementos.length) {
             this.elementos[this.tamanho] = elemento;
             this.tamanho++;
@@ -44,6 +45,7 @@ public class Vetor {
         if (!(posicao >= 0 && posicao < tamanho)) {
             throw new IllegalArgumentException("Posição inválida");
         }
+        this.aumentaCapacidade();
 
         // mover todos os elementos
         for (int i = this.tamanho - 1; i >= posicao; i--) {
@@ -52,6 +54,16 @@ public class Vetor {
 
         this.elementos[posicao] = elemento;
         this.tamanho++;
+    }
+
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            String[] elementosNovos = new String[this.elementos.length*2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
     }
 
     public String busca(int posicao) {
