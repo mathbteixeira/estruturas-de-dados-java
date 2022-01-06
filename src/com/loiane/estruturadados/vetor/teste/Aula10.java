186ca90bcc982ea55d2992dea2e0f54f7352460e
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-05 22:48:17 -0200

    Aula 10 - Generalizando o tipo do vetor

---
+++ src/com/loiane/estruturadados/vetor/teste/Aula10.java
@@ -0,0 +1,31 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+import com.loiane.estruturadados.vetor.VetorGenerico;
+
+public class Aula10 {
+    public static void main(String[] args) {
+        VetorGenerico vetor = new VetorGenerico(3);
+
+        Contato c1 = new Contato("Contato 1", "1234-5678", "contato1@gmail.com");
+        Contato c2 = new Contato("Contato 2", "3333-8888", "contato2@gmail.com");
+        Contato c3 = new Contato("Contato 3", "3444-9999", "contato3@gmail.com");
+
+        Contato c4 = new Contato("Contato 1", "1234-5678", "contato1@gmail.com");
+
+        vetor.adiciona(c1);
+        vetor.adiciona(c2);
+        vetor.adiciona(c3);
+
+        System.out.println("Tamanho = " + vetor.getTamanho());
+
+        int pos = vetor.busca(c4);
+        if (pos > -1) {
+            System.out.println("Elemento existe no vetor");
+        } else {
+            System.out.println("Elemento não existe no vetor");
+        }
+
+        System.out.println(vetor);
+    }
+}
