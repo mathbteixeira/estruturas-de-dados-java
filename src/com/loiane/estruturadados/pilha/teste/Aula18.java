ef5f024586fceabc303318e358096f695e4ec70c
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-15 22:35:27 -0200

    Aula 18 - API Java Stack

---
+++ src/com/loiane/estruturadados/pilha/teste/Aula18.java
@@ -0,0 +1,30 @@
+package com.loiane.estruturadados.pilha.teste;
+
+import com.loiane.estruturadados.pilha.Pilha;
+
+import java.util.Stack;
+
+public class Aula18 {
+    public static void main(String[] args) {
+
+        Pilha<Integer> pilha = new Pilha<>();
+
+        Stack<Integer> stack = new Stack<>();
+
+        System.out.println(stack.isEmpty());
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.size());
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.pop());
+        
+        System.out.println(stack);
+    }
+}
