da34851e96b7ef682787356618368c98f1442f2d
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-15 22:23:36 -0200

    Aula 16 - verificar (espiar) elemento do topo (peek)

--- src/com/loiane/estruturadados/pilha/teste/Aula16.java
+++ src/com/loiane/estruturadados/pilha/teste/Aula16.java
@@ -2,15 +2,18 @@ package com.loiane.estruturadados.pilha.teste;
 
 import com.loiane.estruturadados.pilha.Pilha;
 
-public class Aula15 {
+public class Aula16 {
     public static void main(String[] args) {
 
         Pilha<Integer> pilha = new Pilha<>();
 
-        System.out.println(pilha.estaVazia());
+        System.out.println(pilha.topo());
+        System.out.println(pilha);
 
         pilha.empilha(1);
-
-        System.out.println(pilha.estaVazia());
+        pilha.empilha(2);
+        
+        System.out.println(pilha.topo());
+        System.out.println(pilha);
     }
 }
