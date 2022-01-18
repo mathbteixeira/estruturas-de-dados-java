49d3fc9a9b6ea6d4433ffe109ba088dad4dbff29
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-18 00:15:13 -0200

    Aula 22 - Desenfileirar elemento da fila (dequeue)

--- src/com/loiane/estruturadados/base/EstruturaEstatica.java
+++ src/com/loiane/estruturadados/base/EstruturaEstatica.java
@@ -53,6 +53,16 @@ public class EstruturaEstatica<T> {
         }
     }
 
+    public void remove(int posicao) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        for (int i = posicao; i < tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i+1];
+        }
+        this.tamanho--;
+    }
+
     public int tamanho() {
         return this.tamanho;
     }
--- src/com/loiane/estruturadados/fila/Fila.java
+++ src/com/loiane/estruturadados/fila/Fila.java
@@ -22,4 +22,18 @@ public class Fila<T> extends EstruturaEstatica<T> {
         }
         return this.elementos[0];
     }
+
+    public T desenfileira() {
+
+        final int POS = 0;
+
+        if (this.estaVazia()) {
+            return null;
+        }
+        T elementoASerRemovido = this.elementos[POS];
+
+        super.remove(POS);
+
+        return elementoASerRemovido;
+    }
 }
